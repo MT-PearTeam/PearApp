e4cc64d1b44c7bf521e335b8d93c8147377c9bbf
Santiago <stacero@live.com>
2022-11-22 16:31:57 -0500

    Actualización de toda la App

---
+++ Pear_React/front-react/src/App.js
@@ -0,0 +1,39 @@
+
+import logo from './pear-icon.png';
+import './App.css';
+
+//importamos los componentes
+import CompShowProducts from './Product/ShowProducts';
+import CompCreateProducts from './Product/CreateProduct';
+import CompEditProducts from './Product/EditProduct';
+import CompLogin from './login/login';
+import CompShowStore from './store/ShowStore';
+import CompShowBuy from './store/ShowBuy';
+import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo content-left" alt="logo" />
+        <h2 className='headerTitle'>Pear Team</h2>        
+      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/view' element={<CompShowProducts/>}/>
+        <Route path='/create' element={<CompCreateProducts/>}/>
+        <Route path='/edit/:id' element={<CompEditProducts/>}/>
+        <Route path='/' element={<CompLogin/>}/>
+        <Route path='/store' element={<CompShowStore/>}/>
+        <Route path='/buy' element={<CompShowBuy/>}/>
+      </Routes>      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
